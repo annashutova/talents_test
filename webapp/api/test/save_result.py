ff419c366970fa7d3109924a59f331d080fdef78
annashutova <annashut@inbox.ru>
2025-07-19 18:50:41 +0300

    fixed: user_result endpoint

--- webapp/api/test/save_result.py
+++ webapp/api/test/save_result.py
@@ -1,7 +1,7 @@
 import uuid
 from datetime import datetime, timedelta
 
-from fastapi import Depends
+from fastapi import Depends, HTTPException
 from fastapi.responses import ORJSONResponse
 from fastapi_mail import MessageType, MessageSchema
 from sqlalchemy.ext.asyncio import AsyncSession
@@ -21,6 +21,7 @@ from webapp.logger import logger
 from webapp.models.talents.user import GenderEnum
 from webapp.schema.answer import Responses
 from webapp.models.talents.user_test import StatusEnum
+from webapp.schema.user import UserRegisterRequest
 
 
 @test_router.post('/save-response', response_class=ORJSONResponse)
@@ -32,39 +33,58 @@ async def answer_question(
 
     # 2. Создаем нового пользователя или получаем id существующего (по email).
     user = await get_user_by_email(session, response_data.email)
+    user_pass = hash_password(str(uuid.uuid1()))
     if user is None:
         try:
-            user = await create_user(
-                {
-                    'first_name': response_data.first_name,
-                    'last_name': response_data.last_name,
-                    'surname': response_data.surname,
-                    'email': response_data.email,
-                    'password': hash_password(str(uuid.uuid1())),
-                    'gender': GenderEnum.male if response_data.gender == 'Мужской' else GenderEnum.female,
-                    'date_of_birth': response_data.date_of_birth,
-                },
+            user = await create_user(UserRegisterRequest(
+                first_name=response_data.first_name,
+                last_name=response_data.last_name,
+                surname=response_data.surname,
+                email=response_data.email,
+                password=user_pass,
+                retry_password=user_pass,
+                gender=GenderEnum.male if response_data.gender == 'Мужской' else GenderEnum.female,
+                date_of_birth=response_data.date_of_birth),
                 session,
             )
         except Exception as error:
             logger.error('Error while creating user: %s', str(error))
+            raise HTTPException(500, "Failed to create user")
 
     # 3. Делаем запись в таблице user_test с новым тестированием.
-    test = await post_test(user.id, StatusEnum.finished, session)
+    try:
+        test = await post_test(user.id, StatusEnum.finished, session)
+    except Exception as error:
+        logger.error(f"Failed to create test for user with id = {user.id}: {error}")
+        raise HTTPException(500, "Failed to create test for user")
 
     # 4. Записываем ответы пользователя на вопросы в user_answer.
-    answers = list(map(lambda x: x.update({'test_id': test.id})), response_data.answers)
-    await balk_insert_answers(answers, session)
+    try:
+        for answer in response_data.answers:
+            answer.update({'test_id': test.id})
+        answers = response_data.answers
+        await balk_insert_answers(answers, session)
+    except Exception as error:
+        logger.error(f"Failed to save user answers for test with id = {test.id}: {error}")
+        raise HTTPException(500, "Failed to save user answers")
 
     # 5. Считаем результаты и записываем в таблицу user_result.
-    await post_test_result(test.id, session)
+    try:
+        await post_test_result(test.id, session)
+    except Exception as error:
+        logger.error(f"Failed to calculate and save user results for test with id = {test.id}: {error}")
+        raise HTTPException(500, "Failed to calculate and save user results")
 
     # 6. Создаем новый платеж в таблице invoice и отправляем пользователю на почту платежную ссылку.
-    invoice = await create_invoice(test.id, session)
+    try:
+        invoice = await create_invoice(test.id, session)
+    except Exception as error:
+        logger.error(f"Failed to create invoice for test with id = {test.id}: {error}")
+        raise HTTPException(500, "Failed to create invoice")
 
     payment_link = robokassa.generate_open_payment_link(
         out_sum=invoice.amount,
-        result_url=f'https://clever-inherently-earwig.ngrok-free.app/invoice/confirm',
+        result_url='https://clever-inherently-earwig.ngrok-free.app/invoice/confirm/v2',
         inv_id=invoice.id,
         description=f'Счет на оплату отчета по тесту {test.id} от {invoice.created_at}',
         recurring=False,
